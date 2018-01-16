95a3906d8d779513750026912cd10fc7cc7ad085
veggiedefender <jessejesse123@gmail.com>
2018-01-15 21:58:01 -0500

    test the TOTP generation function
    remove duplicate line
    
    
    replace functions with arrow functions

---
+++ test/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const generateCode = require('..');
+
+describe('TOTP generation', () => {
+  it('should generate correct one-time-passwords', () => {
+    // I've invalidated this secret but the TOTPs are real
+    const secret = '8cr0T+zCLiaSdo1E+Alp7nzAPno=';
+    assert.equal(generateCode(secret, 1516070462), 'WMJ5T');
+    assert.equal(generateCode(secret, 1516070522), 'NH35T');
+    assert.equal(generateCode(secret, 1516070551), 'P5RVN');
+  });
+});
